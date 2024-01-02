98debfa84fa6f86732640bc9b77a58b7c022b2e4
garf <mrhenrysung@gmail.com>
2024-01-02 21:47:46 +1100

    fixed msg typo

--- src/parser.c
+++ src/parser.c
@@ -12,6 +12,6 @@ void parse(char *str1, char *str2)
   printf("enter string 1: ");
   fgets(str1, ENOUGH, stdin);
 
-  printf("enter string2: ");
+  printf("enter string 2: ");
   fgets(str2, ENOUGH, stdin);
 }
